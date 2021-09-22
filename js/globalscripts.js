92d82d5a77fbb624a9ab1835c5e587998feaf366
Hunglikeabee <74536958+Hunglikeabee@users.noreply.github.com>
2021-09-22 14:55:09 +0200

    fixedmenuscrollnavigation

--- js/globalscripts.js
+++ js/globalscripts.js
@@ -1,16 +1,36 @@
-        /* Search bar function */
+/* Search bar function */
 
-        const searchText = document.querySelector(".search-text");
-        const searchButton = document.querySelector(".search-button");
+const searchText = document.querySelector(".search-text");
+const searchButton = document.querySelector(".search-button");
 
-        searchButton.addEventListener("click", () => {
-            let valueInput = searchText.value.trim();
-            window.location.href = `searchresult.html?search=${valueInput}`
-        });
-        searchText.addEventListener("keyup", (e) => {
-            let pressedKey = e.key;
-            let valueInput = searchText.value;
-            if (pressedKey === "Enter") {
-                window.location.href = `searchresult.html?search=${valueInput}`
-            }
-        });
+searchButton.addEventListener("click", () => {
+    let valueInput = searchText.value.trim();
+    window.location.href = `searchresult.html?search=${valueInput}`
+});
+searchText.addEventListener("keyup", (e) => {
+    let pressedKey = e.key;
+    let valueInput = searchText.value;
+    if (pressedKey === "Enter") {
+        window.location.href = `searchresult.html?search=${valueInput}`
+    }
+});
+
+/* Scroll event navigation */
+
+const headerMenu = document.querySelector(".header-menu")
+
+
+
+function scrollFunction() {
+    const scroll = window.scrollY;
+    
+    if (scroll > 130) {
+        headerMenu.classList.add("scrolled-menu")
+
+    }
+    else  if ( scroll < 30){
+        headerMenu.classList.remove("scrolled-menu")
+    }
+}
+
+document.addEventListener("scroll", scrollFunction);
